a93f5584518af287dc3def346a8083a4c8360b99
4905077311 <4905077311@NT876849MT1580.via.varejo.corp>
2023-08-28 15:45:05 -0300

    First commit

---
+++ src/main/java/via/java/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package via.java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Olá mundo");
+
+	}
+
+}
